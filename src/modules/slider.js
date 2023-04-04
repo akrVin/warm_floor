43df3ef2fe087c6fe16a91afb87f7b233af3ccdb
akrVin <akrvin@yandex.ru>
2023-04-04 14:25:11 +0500

    fixed the slider, now it works

--- src/modules/slider.js
+++ src/modules/slider.js
@@ -1,10 +1,16 @@
 /** slider Swiper */
-import Swiper from 'swiper';
+// core version + navigation, pagination modules:
+import Swiper, { Navigation, Pagination } from 'swiper';
 // import Swiper styles
 import 'swiper/css/bundle';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 
 const swiper = new Swiper('.swiper', {
+  // configure Swiper to use modules
+  modules: [Navigation, Pagination],
+
   // ! Optional parameters
   direction: 'horizontal',
   loop: true,
